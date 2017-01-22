e5ffe27a17de0e0a7f2944563757538dc657258a
serjoscha87 <acidnightmare@freenet.de>
2017-01-22 18:01:59 +0100

    updated automation

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,16 @@ var minify = require('gulp-minify');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var babel = require('gulp-babel');
-
-var srcs = ['plugins.js', 'plugins/*.js'];
  
 gulp.task('compress', function() {
-  var path_switch=0;
-  gulp.src(srcs)
+  gulp.src(['plugins.js','plugins/lib/*.js'],{base: '.'})
      .pipe(babel({
          presets: ['es2015']
      }))
     .pipe(minify({
       noSource : true,
-        ext:{
-            min:'.min.js'
-        },
-        exclude: ['tasks'],
-        ignoreFiles: ['.combo.js', '.min.js']
+        ext:{min:'.min.js'}
     }))
-    .pipe(uglify())
-    .pipe(gulp.dest(function(){
-      return ++path_switch >1?'dist/plugins':'dist';
-    }))
-});
-
-/*gulp.task('testUg', function() {
-  gulp.src('plugins.js')
-     .pipe(babel({
-         presets: ['es2015']
-     }))
     .pipe(uglify())
     .pipe(gulp.dest('dist'))
-});*/
+});
